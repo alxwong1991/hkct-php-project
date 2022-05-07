be60765d4aff590e537dd79478f002a77b2deef3
alxwong1991 <alxwong1991>
2022-05-07 19:28:21 +0800

    delete friendlist.js

--- js/friendList.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("#filter-input").on("keyup", function () {
-      var value = $(this).val().toLowerCase();
-      $("#card div").filter(function () {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-      });
-    });
-  });
